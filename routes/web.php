4c85c800a6139dc53a214c7d5141f337f56e6375
Andrey Grytsenko <andreyapg@gmail.com>
2024-05-26 10:09:52 +0300

    Update routes 2024-05-26

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::as('pages:')->group(static function (): void {
+    Route::middleware(['guest'])->group(static function (): void {
+        Route::prefix('auth')->as('auth:')->group(base_path(
+            path: 'routes/web/auth.php',
+        ));
+        Route::prefix('oauth')->as('oauth:')->group(base_path(
+            path: 'routes/web/oauth.php',
+        ));
+    });
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::middleware(['auth'])->group(static function (): void {
+        Route::view('auth/onboarding', 'pages.auth.onboarding')->name('auth:onboarding');
+    });
+
+    Route::middleware(['auth','onboarding'])->group(static function (): void {
+        Route::view('/', 'pages.index')->name('home');
+        Route::prefix('channels')->as('channels:')->group(base_path(
+            path: 'routes/web/channels.php',
+        ));
+    });
 });
